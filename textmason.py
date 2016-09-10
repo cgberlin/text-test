e41608830a922aee70f573efd5411b5d125ec8ff
cody <codyberlin@gmail.com>
2016-09-10 13:02:46 -0700

    added command line arguments for number and message

--- textmason.py
+++ textmason.py
@@ -1,19 +1,21 @@
 import sys
 
 try:
-    number = sys.argv[1] + "@messaging.sprintpcs.com"
+    number = sys.argv[1]
+    msg = sys.argv[2]
     print(number)
 except IndexError:
     print("somethings wrong")
 count = 0
-while (count <= 3):
+while (count <= 30):
         import smtplib
         count += 1
         print(count)
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.starttls()
         server.login("cgspamtext@gmail.com", "transcend123")
-
-        msg = "test!!!"
-        server.sendmail("cgspamtext@gmail.com", number, msg)
+        
+        server.sendmail("cgspamtext@gmail.com", number + "@txt.att.net", msg)
+        server.sendmail("cgspamtext@gmail.com", number + "@messaging.sprintpcs.com", msg)
+        server.sendmail("cgspamtext@gmail.com", number + "@vtext.com", msg)
         server.quit()
