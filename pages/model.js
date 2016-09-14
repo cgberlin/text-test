12296663f50dfbb45bb84e33c14ab1c97aa528af
cody <codyberlin@gmail.com>
2016-09-13 18:56:59 -0700

    added paramaters to get

--- pages/model.js
+++ pages/model.js
@@ -2,8 +2,8 @@
 
 $('#bomb-mason').on('click', function(){
   $.get(
-    url: '/mason',    //your url eg. mypage.php
-    data: ['att', '4242234443', 'test'],   // Parameter you want to pass eg. {version:"5" , language : "php"}
-    success: console.log('success') // function after success
+    '/mason',
+    {'att', '4242234443', 'test'},
+    console.log('success') 
   );
 });
